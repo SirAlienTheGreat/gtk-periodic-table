d398617cd76f1babd5edc713dfbac866f5789640
SirAlienTheGreat <79958059+SirAlienTheGreat@users.noreply.github.com>
2022-02-28 19:04:48 -0700

    Finishing up first release

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
+mod periodic_table_array;
 use gtk4 as gtk;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button,MessageDialog,Label,ButtonsType, DialogFlags, MessageType, Window,Popover,MenuButton};
-use csv;
-use std::fs::File;
-use std::process;
-use std::rc::Rc;
-use std::cell::RefCell;
+use gtk::{Application, ApplicationWindow, Label,Popover,MenuButton,Justification,ArrowType};
 
 fn main() {
     
@@ -18,7 +14,7 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
-    let periodic_table = get_elements();
+    let periodic_table = periodic_table_array::get_periodic_table();
 
     let grid = gtk::Grid::new();
     let window = ApplicationWindow::builder()
@@ -30,33 +26,31 @@ fn build_ui(app: &Application) {
 
     let mut skipped_elements = 0;
 
-    let mut message = MessageDialog::builder()
-            .text("testing")
-            .build();
-        message.connect_response(|dialog, _| dialog.close());
-    message.set_transient_for(Some(&window));
-    
-    let s = Rc::new(RefCell::new(message));
+
 
     // Make Element objects in list "elements" and attach them to the grid
     for i in 0..periodic_table.len(){
-        if periodic_table[i][8] != "".to_string(){ // skip f block
+        // skip f block
             // add new element to elements array
             elements.push(Element::new( &periodic_table[i][1].clone(),
                                         &periodic_table[i][17].clone(),
+                                        periodic_table[i][3].parse().unwrap(),
                                         periodic_table[i][0].parse().unwrap(),
                                         periodic_table[i][2].clone(),
-                                        &s,));
+                                        ));
 
-            // set window as transient for new element dialog
-            elements[i-skipped_elements].dialog.set_transient_for(Some(&window));
-            
+        if periodic_table[i][8] != "".to_string(){ // attach by coordinates if not in f block
             // attach new element to the grid
-            grid.attach(&elements[i-skipped_elements].but,
+            grid.attach(&elements[i].but,
                 periodic_table[i][8].parse().unwrap(),
                 periodic_table[i][7].parse().unwrap(),
                 1,1);
         } else{
+            let mut row = 20;
+            if skipped_elements >=14{
+                row = 21;
+            }
+            grid.attach(&elements[i].but,(skipped_elements%14)+3, row-1,1,1);
             skipped_elements+=1;
         }
     }
@@ -67,11 +61,14 @@ fn build_ui(app: &Application) {
     window.present();
 }
 
-fn get_elements() -> Vec<Vec<String>>{
+/*fn get_elements() -> Vec<Vec<String>>{
     let mut output_record:Vec<csv::StringRecord> =vec![];
     // get list of StringRecord types
     // eg [StringRecord(["1", "Hydrogen", "H", "1.007", "0", . . . ]),]
+    const PROJECT_DIR: Dir = include_dir!("resources");
+
     let file = File::open("PeriodicTable.csv").expect("Couldn't open file");
+    //let file = include_bytes!("/PeriodicTable.csv").unwrap();
     let mut rdr = csv::Reader::from_reader(file);
     for result in rdr.records() {
         match result {
@@ -91,9 +88,9 @@ fn get_elements() -> Vec<Vec<String>>{
             output[i].push(output_record[i].get(j).expect("error").to_string())
         }
     }
-
+    println!("{:?}",output);
     return output;
-}
+}*/
 
 
 // makes variable properties of Element object
@@ -101,38 +98,34 @@ struct Element{
     name:String,
     electronegativity:String,
     atomic_number:i32,
+    mass:f32,
     symbol:String,
     but:MenuButton,
-    dialog:gtk4::MessageDialog,
     popover_text:gtk4::Label,
     popover:gtk4::Popover,    
 }
 
 // makes object type "MakeButton" with 2 functions that must be defined per-object
 trait MakeButton{
-    fn make_button(&mut self,ref_cell:&Rc<RefCell<MessageDialog>>);
-    fn new(name:&str,electronegativity:&str,atomic_number:i32,symbol:String,ref_cell:&Rc<RefCell<MessageDialog>>) -> Element;
+    fn make_button(&mut self);
+    fn new(name:&str,electronegativity:&str,mass:f32,atomic_number:i32,symbol:String) -> Element;
 }
 
 // makes Element object "Element" a "MakeButton" type
 impl MakeButton for Element{
-    fn make_button(&mut self,ref_cell:&Rc<RefCell<MessageDialog>>){
-        let label = self.symbol.clone();
-        let number = self.atomic_number.clone();
-        /*let message = MessageDialog::builder()
-            .text("testing")
-            .build();
-        message.connect_response(|dialog, _| dialog.close());*/
-        
-        let s2 = Rc::clone(ref_cell);
+    fn make_button(&mut self){
+
         
         //build popover label
         let popover_labal = Label::builder()
             .label(&[
-                self.name.clone(),"\n".to_string(),
-                self.symbol.clone(),"\n".to_string(),
+                self.name.clone()," (".to_string(),
+                self.symbol.clone(),")\n".to_string(),
+                "#".to_string(),self.atomic_number.to_string(),"\n".to_string(),
+                "Mass: ".to_string(),self.mass.to_string()," amu\n".to_string(),
                 "Electronegativity: ".to_string(),self.electronegativity.clone(),"\n".to_string(),
-                "#".to_string(),self.atomic_number.to_string()].join("").as_str())
+                ].join("").as_str())
+            .justify(Justification::Center)
             .build();
         self.popover_text = popover_labal;
         
@@ -144,30 +137,35 @@ impl MakeButton for Element{
             .has_arrow(true)
             .build();
         self.popover = popover;
-        
+
         //build button for popover
         let but = MenuButton::builder()
             .label(&self.symbol)
             .hexpand(true)
             .vexpand(true)
+            .direction(ArrowType::None)
             .popover(&self.popover)
             .build();
         
-        
         self.but = but;
     }
-    fn new(name:&str,electronegativity:&str,atomic_number:i32,symbol:String,ref_cell:&Rc<RefCell<MessageDialog>>) -> Element{
+    fn new(name:&str,electronegativity:&str,mass:f32,atomic_number:i32,symbol:String) -> Element{
+        let mut electronegativity_with_na = electronegativity;
+        if electronegativity_with_na == ""{
+            electronegativity_with_na = "N/A";
+        }
+
         let mut output:Element = Element{
             name:name.to_string(),
-            electronegativity:electronegativity.to_string(),
-            atomic_number:atomic_number,
-            symbol:symbol,
+            electronegativity:electronegativity_with_na.to_string(),
+            mass,
+            atomic_number,
+            symbol,
             but:MenuButton::new(),
-            dialog:MessageDialog::builder().build(),
             popover_text:gtk4::Label::builder().label("testing").build(),
             popover:Popover::builder().build(),
         };
-        output.make_button(&ref_cell);
+        output.make_button();
         return output;
     }
 }
